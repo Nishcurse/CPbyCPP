cb45dccf3101b4a60160274e6f89dbd38ac16c79
Utkarsh Mudgal <notingislive@gmail.com>
2023-09-05 06:34:15 +0530

    Compatative Proogramming Repo Init (On codeForces) as of 05/09/2023

---
+++ Learning/main.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    // int a = 32; // Global variable
+    /* OVERFLOW */
+    // Ranges of Data Types
+    /* -10^9 < int < 10^9 */
+    /* -10^12 < long int < 10^12 */
+    /* -10^18 < long long int < 10^18 */
+    // Loops in CPP 
+    // for (int i = 0; i < a; i++ /* as long as its true*/)
+    // {
+    //     int c = 3; // Local Variable only accessible within the scoop
+    //     if(i==3){
+    //     break; // exit to the last for loop Always no matter how many if loops you have used
+    //     }
+    // } 
+    // while (a > 33 /* untill its true*/)
+    // {
+    //     cout << "this code will work" << endl;
+    //     a++;
+    // }
+    // do
+    // {
+    //     /* code */
+    // } while (/* condition */); // will run atleast once then untill cond is true
+
+    
+    // Strings(string) in cpp
+    /*
+    string a,b; // Collection of the characters
+    cin>>a>>b; // stdIN for a String Breaks After " " or "\n"--> new line 
+    cout<<a<<endl;
+    */
+   // String Methods
+    string x= "Utkarsh ";
+    string y = "Mudgal" ;
+    string result = x + y; 
+    // on cout will give "Utkarsh MUdgal" 
+    cout<<result<<endl;
+    cout<<(x==y)<<endl; //can compare two string isEqual or notEqual-->caseSensitive
+    // x[index] will return the char at that index new char can also be assigned 
+    cout<<sizeof(x)<<endl;//can return the size of string
+    cout<<x.length()<<endl;//can return the length of string
+    getline(cin, y); //stdIn as Whole Line
+    
+    return 0;
+}
