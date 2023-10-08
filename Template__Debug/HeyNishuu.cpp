ae3da35bf6d82d9b28bf8bd76f7cfb8b653c326c
Utkarsh Mudgal <notingislive@gmail.com>
2023-10-08 10:27:02 +0530

    Making New Personal Template For Contests

---
+++ Template__Debug/HeyNishuu.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+#define HeyNishuu ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0); 
+#define TestCase int t;cin>>t; while (t--)
+#define for(a,n) for(int i=a;i<n;i++) 
+typedef long long int ll;
+using namespace std;
+int main () {
+    HeyNishuu
+    TestCase {
+        for(0,4){
+            cout<<1<<"\n";
+        }
+        
+    }
+    return 0;
+}
---
+++ Template__Debug/temp.cpp
@@ -0,0 +1,21 @@
+/* "Problem Name n Code" By Utkarsh(Nishu_--_
+                                          __  )*/
+#include <bits/stdc++.h>
+#pragma GCC optimize("Ofast")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,tune=native")
+typedef long long ll;
+#define HeyNishu                      \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(0);                       \
+    cout.tie(0);
+using namespace std;
+int main()
+{
+    HeyNishu int t;
+    cin >> t;
+    while (t--)
+    {
+    
+    }
+    return 0;
+}
